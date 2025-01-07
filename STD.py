bf397a3ace9773d4f0dae51de9f2473ba4090572
Harry Collins <121592185+scrundae@users.noreply.github.com>
2025-01-07 13:22:35 +1000

    Rewrite begins

---
+++ STD.py
@@ -0,0 +1,7 @@
+# Commands
+
+def PrintCom(arg1):
+    print(arg1)
+
+def InputCom(arg1):
+    return input(arg1)
--- p7c.py
+++ p7c.py
@@ -1,47 +1,20 @@
-import re
+import STD
 import sys
+import shlex
 
-KEYWORDS = {
-    'Sub': 'void',  
-    'Integer': 'int',
-    'Character': 'char',
-    'While': 'while',
-    'Do': 'do',
-    'Match': 'match',
-    'Case': 'case',
-    'Default': 'default',
-    'Proceed': 'continue',
-    'Start': '{',
-    'End': '}',
-    'If': 'if',
-    'Else': 'else',
-    'Return': 'return',
-    'For': 'for',
-    'Public': 'public',
-    'Private': 'private',
-    'Protected': 'protected',
-    'Bucket': 'class',
+class interpreter:
+    def __init__(self):
+        variables = {}
+    def execute_script(filename):
+        f = open(filename, 'r')
+        ftext = f.read()
+        for line in ftext.split('\n'):
+            flex = shlex.split(line)
+            if flex[0] == "Print":
+                STD.PrintCom(flex[1])
+            elif flex[0] == "Input":
+                STD.InputCom(flex[1])
+        
 
-}
-
-def convert_to_c(pseudo_code, keywords):
-    # Split the input pseudo-code into lines for easier processing
-    lines = pseudo_code.splitlines()
-    c_code = []
-
-    # Replace each keyword in the line with its corresponding C equivalent
-    for line in lines:
-        for key, value in keywords.items():
-            line = line.replace(key, value)
-        c_code.append(line)
-
-    # Combine the converted lines back into a single string
-    return "\n".join(c_code)
-
-fl = open(sys.argv[1], 'r')
-out = open(sys.argv[2], 'w')
-
-pseudo_code = fl.read()
-
-converted_c_code = convert_to_c(pseudo_code, KEYWORDS)
-out.write(converted_c_code)
+fn = sys.argv[1]
+interpreter.execute_script(fn)
